8e57d12ee98bd35ab53868153b6b08457f510001
student <student>
2016-08-31 12:57:31 +0530

    Version 1.0

---
+++ add.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void add(int a ,int b){
+int c ;
+c = a+b ;
+printf("\n Sum = %d",c);
+	
+
+}
+void sub(int a, int b){
+int c ;
+c= a-b;
+printf("\nsub = %d ",c);
+
+}
+
+
+
+int main()
+{
+   int a, b, c;
+    
+       printf("Enter two numbers to add\n");
+          scanf("%d %d",&a,&);
+	  add(a,b);
+	  sub(a,b);
+	  
+	  } 
